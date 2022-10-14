629f8b56a155f1e79bc02fd8585cebe352f187bd
Allen Gilbert <allen.gilbert@doane.edu>
2022-10-14 10:08:44 -0500

    Continued WIP on support for cropping video

--- Sources/Mantis/CropView/ImageContainer.swift
+++ Sources/Mantis/CropView/ImageContainer.swift
@@ -23,43 +23,53 @@ private class LDVideoView: UIView {
 
 class ImageContainer: UIView {
 
-//    lazy private var imageView: UIImageView = {
-//        let imageView = UIImageView(frame: bounds)
-//        imageView.layer.minificationFilter = .trilinear
-//        imageView.accessibilityIgnoresInvertColors = true
-//        imageView.contentMode = .scaleAspectFit
-//
-////        addSubview(imageView)
-//
-//        return imageView
-//    }()
+    lazy private var imageView: UIImageView = {
+        let imageView = UIImageView(frame: bounds)
+        imageView.layer.minificationFilter = .trilinear
+        imageView.accessibilityIgnoresInvertColors = true
+        imageView.contentMode = .scaleAspectFit
 
-    lazy private var videoView: LDVideoView = {
-        let videoView = LDVideoView(frame: bounds)
-        addSubview(videoView)
-        return videoView
+        addSubview(imageView)
+
+        return imageView
     }()
 
     var image: UIImage? {
         didSet {
-//            imageView.frame = bounds
-//            imageView.image = image
-//
-//            imageView.isUserInteractionEnabled = true
+            imageView.frame = bounds
+            imageView.image = image
+
+            imageView.isUserInteractionEnabled = true
         }
     }
 
+    lazy private var videoView: LDVideoView = {
+        let videoView = LDVideoView(frame: bounds)
+        insertSubview(videoView, aboveSubview: imageView)
+        return videoView
+    }()
+
+    private var videoLooper: AVPlayerLooper?
+
+    // TODO: eventually, it'd be nice to not require setting image (and setting up imageView) to crop video
     var video: URL? {
         didSet {
             if let video {
-                let player = AVPlayer(url: video)
+                // set up looping video player
+                let item = AVPlayerItem(url: video)
+                let player = AVQueuePlayer(playerItem: item)
+                videoLooper = AVPlayerLooper(player: player, templateItem: item)
                 player.isMuted = true // TODO: set per current user preference
-                player.actionAtItemEnd = .none
                 videoView.player = player
+                videoView.playerLayer.videoGravity = .resizeAspect
+                // these two lines mitigate weird border artifacts that sometimes occur without them; memory impact is negligible.
+                // TODO: figure out if these are still necessary when running iOS 14+
+//                videoView.playerLayer.shouldRasterize = true
+//                videoView.playerLayer.rasterizationScale = UIScreen.main.scale
             } else {
                 videoView.player?.rate = 0
                 videoView.player = nil
-                // TODO: tear down other things?
+                videoLooper = nil
                 videoView.removeFromSuperview()
             }
         }
@@ -67,8 +77,11 @@ class ImageContainer: UIView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-//        imageView.frame = bounds
-        videoView.frame = bounds
+        imageView.frame = bounds
+
+        if video != nil {
+            videoView.frame = bounds
+        }
     }
     
     func contains(rect: CGRect, fromView view: UIView, tolerance: CGFloat = 1e-6) -> Bool {
